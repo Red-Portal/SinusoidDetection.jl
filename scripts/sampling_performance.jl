215e0978ccd85216ab9204bbb2817d86c1d70a13
Ray Kim <msca8h@naver.com>
2023-12-08 01:38:54 +0000

    fix long chain sampling experiment

--- scripts/sampling_performance.jl
+++ scripts/sampling_performance.jl
@@ -29,11 +29,36 @@ using Statistics
 #     estimated_autocov = computeMoments(support = m$support, weights = m$weights, M = 100)
 # end
 
-mutable struct TrackedModel{
-    model <: SinusoidDetection.AbstractSinusoidModel
-} <: SinusoidDetection.AbstractSinusoidModel
-    evals
-    model
+mutable struct TrackedModel{M} <: SinusoidDetection.AbstractSinusoidModel
+    model::M
+    evals::Int
+end
+
+function ReversibleJump.local_proposal_sample(
+    rng    ::Random.AbstractRNG,
+    tracked::TrackedModel,
+    prop   ::SinusoidLocalProposal
+)
+    ReversibleJump.local_proposal_sample(rng, tracked.model, prop)
+end
+
+function ReversibleJump.local_proposal_logpdf(
+    tracked::TrackedModel, prop, θ, j
+)
+    ReversibleJump.local_proposal_logpdf(tracked.model, prop, θ, j)
+end
+
+function ReversibleJump.local_insert(tracked::TrackedModel, θ, j, θj)
+    ReversibleJump.local_insert(tracked.model, θ, j, θj)
+end
+
+function ReversibleJump.local_deleteat(tracked::TrackedModel, θ, j)
+    local_deleteat(tracked.model, θ, j)
+end
+
+function ReversibleJump.logdensity(tracked::TrackedModel, θ)
+    tracked.evals += 1
+    ReversibleJump.logdensity(tracked.model, θ)
 end
 
 function run_sampling(
@@ -47,6 +72,7 @@ function run_sampling(
     initial_params = [init_delta2]
     initial_order  = 0
     samples, stats = ReversibleJump.sample(
+        rng,
         rjmcmc,
         model,
         n_samples + n_burn,
@@ -54,26 +80,32 @@ function run_sampling(
         initial_params,
         show_progress=true
     )
-    order_chain = [stat.order for stat in stats]
-    last(order_chain, n_samples)
+    samples = last(samples, n_samples)
+    stats   = last(stats, n_samples)
+    k_post  = [stat.order for stat in stats]
+    ω_post  = [first(sample) for sample in samples]
+    k_post, ω_post
 end
 
 function run_config(rng, config)
     N  = 64
     Δω = π/15
-    σ² = 3.0
+    σ² = 1.0
     ω  = (0.5 .+ Δω*(0:config.ntargets-1)) |> collect
     δ² = 10^(config.snr/10)
-    y  = SinusoidDetection.sample_signal(rng, ω, N, σ², δ²)
+    a  = fill(σ²*sqrt(δ²), config.ntargets*2)
+    y  = SinusoidDetection.sample_signal_fixed_amplitude(rng, ω, a, N, σ²)
 
-    n_burn    = 10
-    n_samples = 100
+    n_burn    = 1000
+    n_samples = 100000
     n_anneal  = 4
 
     ν0    = 0.0
     γ0    = 0.0
     α_δ²  = 1/2
     β_δ²  = N/2
+    #α_δ²  = 2.0
+    #β_δ²  = 0.1
     α_λ   = 0.1
     β_λ   = 0.1 
     k_max = floor(Int, (N-1)/2)
@@ -90,7 +122,7 @@ function run_config(rng, config)
     elseif config.mcmc == :indep_slice
         #
         model = SinusoidUnknownSNR(y, ν0, γ0, α_δ², β_δ², prior)
-        mcmc  = SliceSinusoid(SliceSteppingOut(), model, 1.0, 1.0)
+        mcmc  = SliceSinusoid(SliceSteppingOut(), model, 1.0, 10.0)
         jump  = IndepJumpProposal(prop)
         model, 1.0, jump, mcmc
     elseif config.mcmc == :indep_slice_reparam
@@ -102,7 +134,7 @@ function run_config(rng, config)
     elseif config.mcmc == :ais_slice
         #
         model = SinusoidUnknownSNR(y, ν0, γ0, α_δ², β_δ², prior)
-        mcmc  = SliceSinusoid(SliceSteppingOut(), model, 1.0, 1.0)
+        mcmc  = SliceSinusoid(SliceSteppingOut(), model, 1.0, 10.0)
 	jump  = AnnealedJumpProposal(n_anneal, prop, path)
         model, 1.0, jump, mcmc
     elseif config.mcmc == :ais_slice_reparam
@@ -119,8 +151,13 @@ function run_config(rng, config)
         ReversibleJump.NonReversibleJumpMCMC(jump, mcmc; jump_rate=0.8)
     end
     
-    samples = run_sampling(rng, model, rjmcmc, n_burn, n_samples, init_delta2)
-    Dict("samples" => samples)
+    tracked         = TrackedModel(model, 0)
+    k_post, δ²_post = run_sampling(rng, tracked, rjmcmc, n_burn, n_samples, init_delta2)
+    Dict(
+        "delta2" => δ²_post,
+        "k"      => k_post,
+        "evals"  => tracked.evals
+    )
 end
 
 function main()
@@ -132,7 +169,7 @@ function main()
     # SNR         : 0dB, 5dB, 10dB
     # num targets : 1, 5, 10
 
-    snrs      = [(snr       = snr,)      for snr       in [0, 5, 10]]
+    snrs      = [(snr       = snr,)      for snr       in [0, 1, 5]]
     ntargets  = [(ntargets  = ntargets,) for ntargets  in [1, 5, 10]]
     rjmcmcs   = [(rjmcmc = rjmcmc,)      for rjmcmc    in [:rjmcmc, :nrjmcmc]]
     mcmcs     = [
@@ -149,7 +186,11 @@ function main()
 
     for config in configs
         fname = savename("pre", config)
-        DrWatson.produce_or_load(config, datadir("exp_raw"), prefix="longchain") do config′
+        DrWatson.produce_or_load(
+            config,
+            projectdir("../data/exp_raw"),
+            prefix="longchain"
+        ) do config′
             run_config(rng, config′)
         end
     end
